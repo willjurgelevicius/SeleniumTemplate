9b7a3cc23d2396dc871fab99d793420e2ed0e168
Vilius <vj@frontit.dk>
2019-01-03 20:52:54 +0200

    fixing trivial project settings

--- skycopUI/SkycopSteps.feature.cs
+++ skycopUI/SkycopSteps.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace BAUnipark
+namespace SkycopUI
 {
     using TechTalk.SpecFlow;
     
